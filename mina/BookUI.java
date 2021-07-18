74890c85828088199b261a851e7d2f23ee9bce59
mina <alsdk9458@naver.com>
2021-07-18 16:19:27 +0900

    2021.07.18 김민아

--- mina/BookUI.java
+++ mina/BookUI.java
@@ -37,10 +37,10 @@ public class BookUI {
 				System.out.print("비밀번호 : ");
 				pwd = scan.next();
 
-			} else if (!pwd.equals(vo.getPwd())) {
+			} else if (!pwd.equals(vo.getPwd()) || !user_id.equals(vo.getUser_id())) {
 
 				error++;
-				System.out.println("비밀번호 불일치. 다시 로그인 하세요.");
+				System.out.println("아이디 혹은 비밀번호 불일치. 다시 로그인 하세요.");
 
 				System.out.println("********로그인 " + error + "회 재시도********");
 				System.out.print("아이디 : ");
@@ -50,43 +50,22 @@ public class BookUI {
 
 				continue;
 
-			} else if (!user_id.equals(vo.getUser_id())) {
-				
-				error++;
-				System.out.println("아이디 불일치. 다시 로그인 하세요.");
-
-				System.out.println("********로그인 " + error + "회 재시도********");
-				System.out.print("아이디 : ");
-				user_id = scan.next();
-				System.out.print("비밀번호 : ");
-				pwd = scan.next();
-
-				continue;
-
-			}else if(error == 5) {
+			} else if (error == 5) {
 				System.out.println("비밀번호를 5회이상 틀리셨습니다.");
 				System.out.println("비밀번호 초기화를 진행하시겠습니까? Y:1 N:2");
-				
+
 				int choice = scan.nextInt();
-					if(choice == 1) {
-						 System.out.println("새로운 비밀번호를 입력하세요");
-						 vo.setPwd(pwd = scan.next());
-					} else {
-						break;
-					}
-				//하는 중!
-				
-				
+				if (choice == 1) {
+					System.out.println("새로운 비밀번호를 입력하세요");
+					vo.setPwd(scan.next());
+				} else {
+					break;
+				}
+
 			} else {
 				System.out.println(userName + "님 환영합니다.");
 				break;
 			}
-			/*
-			 * BookStore\exam01\ex1.PNG 이미지 참고!! 1.로그인 2.아이디 , 비밀번호 실패 시 error카운트 1씩 증가 3.
-			 * error 카운트 5이상일 경우 비밀번호 초기화 세팅 4. 변경 된 비밀번호로 로그인 성공 4-1 로그인 성공 시 {이름}님 환영합니다 후
-			 * break; 5.자유주제 선정하여 개발하기!
-			 */
 		}
 	}
-
 }
